d79c57c97f862d71e544aebcc8faad4e82596111
danielgouveiah <nel_hue@hotmail.com>
2022-08-02 19:21:42 -0300

    Seção de acadêmico criada

--- js/modules/animar-scroll.js
+++ js/modules/animar-scroll.js
@@ -1,6 +1,6 @@
 export default function initAnimarScroll() {
   const sections = document.querySelectorAll("[data-anime='scroll']");
-  const halfWindow = window.innerHeight * 0.8;
+  const halfWindow = window.innerHeight * 0.5;
 
   function animarScroll() {
     sections.forEach((section) => {
