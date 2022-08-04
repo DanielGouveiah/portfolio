b1170167acfc47dc53e07833f6f2adbcb0a4549c
danielgouveiah <nel_hue@hotmail.com>
2022-08-04 20:10:06 -0300

    Adicionado um scroll quando botão de ver menos é clicado

--- js/modules/show-more.js
+++ js/modules/show-more.js
@@ -2,16 +2,30 @@ export default class ShowMore {
   constructor(btn, lista) {
     this.showBtn = document.querySelector(btn);
     this.lista = document.querySelector(lista);
+    this.listaParent = this.lista.parentElement;
     this.events = ["touch", "click"];
     this.showMore = this.showMore.bind(this);
+    this.goTop = this.goTop.bind(this);
+  }
+
+  goTop() {
+    this.listaParent.scrollIntoView({ behavior: "smooth" });
   }
 
   showMore() {
     this.lista.classList.toggle("ativo");
     if (this.lista.classList.contains("ativo")) {
       this.showBtn.innerHTML = "Ver menos";
+      this.events.forEach((userEvent) => {
+        this.showBtn.addEventListener(userEvent, this.goTop);
+      });
     } else {
       this.showBtn.innerHTML = "Ver mais";
+      setTimeout(() => {
+        this.events.forEach((userEvent) => {
+          this.showBtn.removeEventListener(userEvent, this.goTop);
+        });
+      }, 0);
     }
   }
 
